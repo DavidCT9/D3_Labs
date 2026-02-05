437de2c5847e14dcdf50370deb1214e358e8099b
DavidCT9 <0253018@up.edu.mx>
2026-02-05 13:36:03 -0600

    First exercise done

--- 1/js/main.js
+++ 1/js/main.js
@@ -2,4 +2,6 @@ var svg = d3.select("#chart-area").append("svg").attr("width",400).attr("height"
 var circle = svg.append("circle").attr("cx", 100).attr("cy",250).attr("r",70).attr("fill","blue");
 var rect = svg.append("rect").attr("x",20).attr("y",20).attr("width",20).attr("height",20).attr("fill","red");
 
+var elipse = svg.append("ellipse").attr("cx",100).attr("cy",300).attr("rx",30).attr("ry",55).attr("fill","green");
+
 
